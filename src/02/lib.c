1b227c898e0253833a5ee970cd37933c2c1d58df
Hiromichi Iiduka <hiromichi@rx7fc.com>
2010-12-18 07:45:15 +0900

    update libc.

--- src/02/lib.c
+++ src/02/lib.c
@@ -18,18 +18,60 @@ void* memcpy(void* dst, const void* src, long len)
     for(; len>0; --len){
         *(d++) = *(s++);
     }
-    return src;
+    return dst;
 }
 
-void* memcmp(const void* b1, const void* b2, long len)
+int memcmp(const void* b1, const void* b2, long len)
 {
-    const char* p1 = b1, p2 = b2;
+    const char* p1 = b1, *p2 = b2;
     for(; len>0; --len){
         if(*p1 != *p2){
             return (*p1 > *p2)?1:-1;
         }
         ++p1;
         ++p2;
+    }
+    return 0;
+}
+
+int strlen(const char* s)
+{
+    int len;
+    for(len=0; *s; ++s, ++len);
+    return len;
+}
+
+char* strcpy(char* dst, const char* src)
+{
+    char* d = dst;
+    for(;; ++dst, ++src){
+        *dst = *src;
+        if(!*src) break;
+    }
+    return d;
+}
+
+int strcmp(const char* s1, const char* s2)
+{
+    while(*s1 || *s2){
+        if(*s1 != *s2){
+            return (*s1 > *s2)?1:-1;
+        }
+        ++s1;
+        ++s2;
+    }
+    return 0;
+}
+
+int strncmp(const char* s1, const char* s2, int len)
+{
+    while((*s1 || *s2) && (len > 0)){
+        if(*s1 != *s2){
+            return (*s1 > *s2)?1:-1;
+        }
+        ++s1;
+        ++s2;
+        --len;
     }
     return 0;
 }
@@ -49,3 +91,25 @@ int puts(unsigned char* str){
     return 0;
 }
 
+int putxval(unsigned long value, int column)
+{
+    char buf[9];
+    char *p;
+
+    p = buf + sizeof(buf) - 1;
+    *(p--) = '\0';
+
+    if(!value && !column){
+        ++column;
+    }
+
+    while(value || column){
+        *(p--) = "0123456789abcdef"[value & 0xF];
+        value >>= 4;
+        if(column) --column;
+    }
+
+    puts((unsigned char*)(p+1));
+
+    return 0;
+}
--- src/02/lib.h
+++ src/02/lib.h
@@ -1,7 +1,17 @@
 #ifndef _LIB_H_INCLUDE_
 #define _LIB_H_INCLUDE_
 
+void* memset(void* b, int c, long len);
+void* memcpy(void* dst, const void* src, long len);
+int memcmp(const void* b1, const void* b2, long len);
+int strlen(const char* s);
+char* strcpy(char* dst, const char* src);
+int strcmp(const char* s1, const char* s2);
+int strncmp(const char* s1, const char* s2, int len);
+
 int putc(unsigned char c);
 int puts(unsigned char *str);
+
+int putxval(unsigned long value, int column);
 
 #endif /*_LIB_H_INCLUDE_*/

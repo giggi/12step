1888a505be60108a1ee984f4046bc42d42287026
Hiromichi Iiduka <hiromichi@rx7fc.com>
2011-02-15 18:18:51 +0900

    added source of Chapter 11.

---
+++ src/11/os/test11_1.c
@@ -0,0 +1,41 @@
+#include "defines.h"
+#include "kozos.h"
+#include "lib.h"
+
+int
+test11_1_main(int argc, char* argv[])
+{
+    char* p;
+    int size;
+    
+    puts("test11_1 started.\n");
+
+    /* receive static mem */
+    puts("test11_1 recv in.\n");
+    kz_recv(MSGBOX_ID_MSGBOX1, &size, &p);
+    puts("test11_1 recv out.\n");
+    puts(p);
+
+    /* receive dynamic mem */
+    puts("test11_1 recv in.\n");
+    kz_recv(MSGBOX_ID_MSGBOX1, &size, &p);
+    puts("test11_1 recv out.\n");
+    puts(p);
+    kz_kmfree(p);
+
+    /* send static mem */
+    puts("test11_1 send in.\n");
+    kz_send(MSGBOX_ID_MSGBOX2, 15, "static memory\n");
+    puts("test11_1 send out.\n");
+    
+    /* send dynamic mem */
+    p = kz_kmalloc(18);
+    strcpy(p, "allocated memory\n");
+    puts("test11_1 send in.\n");
+    kz_send(MSGBOX_ID_MSGBOX2, 18, p);
+    puts("test11_1 send out.\n");
+    
+    puts("test11_1 exit.\n");
+    
+    return 0;
+}
